ac532b79a4b1dab90b7b524199c76becaf91990a
Faisal <mirzaf317@gmail.com>
2024-11-10 00:23:32 +0530

    added some functionality

--- src/App.js
+++ src/App.js
@@ -4,25 +4,38 @@ import { useState } from "react";
 const App = () => {
   const [tasks, setTasks] = useState([]);
 
-  const addTask = (newTask) => {
-    newTask = prompt("Enter a new task: ");
-    if (newTask != "") {
-      setTasks([...tasks, newTask]);
+  const addTask = () => {
+    const newTask = prompt("Enter a new task: ");
+    if (newTask) {
+      setTasks([...tasks, { text: newTask, completed: false }]);
     }
   };
 
+  const taskComplete = (index) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
   const removeTask = () => {
-    console.log(tasks);
     setTasks(tasks.slice(0, -1));
   };
 
   return (
     <>
       <h1 id="title">My To-Do List</h1>
-      <div id="task-list">
-        <ul>
+      <div id="task-container">
+        <ul id="task-list">
           {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
+            <li
+              key={index}
+              onClick={() => taskComplete(index)}
+              className={task.completed ? "completed" : ""}
+            >
+              <span>{task.text}</span>
+            </li>
           ))}
         </ul>
       </div>
