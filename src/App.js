2682e65ae75a880bb564ae99889b55d451622d58
Faisal <mirzaf317@gmail.com>
2024-11-14 01:11:34 +0530

    Added functionality and adjusted layout

--- src/App.js
+++ src/App.js
@@ -13,9 +13,16 @@ const App = () => {
 
   const toggleTaskComplete = (index) => {
     setTasks((prevTasks) =>
-      prevTasks.map((task, i) =>
-        i === index ? { ...task, completed: !task.completed } : task
-      )
+      prevTasks.map((task, i) => {
+        // console.log(task.completed);
+        return i === index ? { ...task, completed: !task.completed } : task;
+      })
+    );
+    console.log(
+      "task Name: ",
+      tasks[index].text,
+      "completed: ",
+      tasks[index].completed
     );
   };
 
@@ -28,46 +35,77 @@ const App = () => {
       <h1 className="title">My To-Do List</h1>
       <div className="container">
         <div className="task-list">
-          {tasks.map((task, index) => (
-            <div className="task-container" key={index}>
-              <div className="checkBox">
-                <input
-                  type="checkbox"
-                  onClick={() => {
-                    toggleTaskComplete(index);
-                  }}
-                />
+          {/*code will be pasted here*/}
+          {tasks.map((task, index) => {
+            return task.completed ? (
+              ""
+            ) : (
+              <div className="task-container" key={index}>
+                <div className="checkBox">
+                  <input
+                    type="checkbox"
+                    onClick={() => {
+                      console.log(
+                        "We are in 1st onclick and its index is: ",
+                        index
+                      );
+                      toggleTaskComplete(index);
+                    }}
+                  />
+                </div>
+                <div className={"task-name"}>
+                  <h4>{task.text}</h4>
+                </div>
+                <div className="remove-container">
+                  <span
+                    onClick={() => {
+                      removeTask(index);
+                    }}
+                  >
+                    &#x2715;
+                  </span>
+                </div>
               </div>
-              <div className="task-name">
-                <h4>{task.text}</h4>
-              </div>
-              <div className="remove-container">
-                <span
-                  onClick={() => {
-                    removeTask(index);
-                  }}
-                >
-                  &#x2715;
-                </span>
-              </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
       <button onClick={addTask}>Add Task</button>
       <div className="container">
         <h1 className="title">Completed Task</h1>
         <div className="task-list">
-          {tasks.map((task, index) => (
-            <div className="task-container" key={index}>
-              <div className="checkBox">
-                <input type="checkbox" />
+          {tasks.map((task, index) => {
+            return task.completed ? (
+              <div className="task-container" key={index}>
+                <div className="checkBox">
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onClick={() => {
+                      console.log(
+                        "We are in 2nd onclick and its index is: ",
+                        index
+                      );
+                      toggleTaskComplete(index);
+                    }}
+                  />
+                </div>
+                <div
+                  className={
+                    task.completed ? "task-name completed" : "task-name"
+                  }
+                >
+                  <h4>{task.text}</h4>
+                </div>
+
+                <div className="remove-container">
+                  <span>&#x2715;</span>
+                </div>
               </div>
-              <div className="task-name">
-                <h4>{task.text}</h4>
-              </div>
-            </div>
-          ))}
+            ) : (
+              ""
+            );
+          })}
         </div>
       </div>
     </>
